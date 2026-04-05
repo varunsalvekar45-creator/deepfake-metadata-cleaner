72a1ae12618ad1d8f37ab3694282e2112092b2e4
varunsalvekar45-creator <varunsalvekar45@gmail.com>
2026-04-05 15:52:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,8 @@ def home():
     <p>{{result}}</p>
     """, result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
